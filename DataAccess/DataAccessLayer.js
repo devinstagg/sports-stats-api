703096ddfe153008a21bf020034f6fbe8ad57e9f
Devin Stagg <devinstagg7@gmail.com>
2020-04-07 18:57:21 -0600

    Adding Data Access Layer

---
+++ DataAccess/DataAccessLayer.js
@@ -0,0 +1,19 @@
+const { MongoClient } = require('mongodb');
+const uri = process.env.ATLAS_CONNECTION;
+
+const getConnectedClient = async () => {
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    return await client.connect()
+}
+
+const getAllNflTeams = async () => {
+    const client = await getConnectedClient()
+
+    const collection = await client.db("sports-stats").collection("nfl-teams");
+
+    const data = await collection.find().toArray()
+
+    await client.close();
+
+    return data
+}
