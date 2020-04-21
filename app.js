62ec1124184b49aae4152d54f6d3b558b1492049
Devin Stagg <devinstagg7@gmail.com>
2020-04-20 21:03:00 -0600

    cors

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
   res.header('Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+      'Origin, X-Requested-With, Content-Type, Accept')
   
   next()
 })
