245206d505215bec80f501e0f9f7bb32da638084
Devin Stagg <devinstagg7@gmail.com>
2020-04-21 18:17:58 -0600

    updating CORs

--- app.js
+++ app.js
@@ -8,19 +8,19 @@ const nflTeamsRouter = require('./routes/nfl-teams');
 
 var app = express();
 
-app.use(express.json())
+app.use(logger('dev'));
+app.use(express.json());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
   res.header('Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept')
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization')
   
   next()
 })
 
-app.use(logger('dev'));
-app.use(express.json());
+
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
